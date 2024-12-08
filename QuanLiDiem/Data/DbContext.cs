3b96187fada472de4bd4cb4b02a1d2ab25fb253f
phuchung123 <tranhung.2612va@gmail.com>
2024-12-08 10:45:02 +0700

    hung update

---
+++ QuanLiDiem/Data/DbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QuanLiDiem.Models;
+
+namespace QuanLiDiem.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DanhSachSinhVien> DanhSachSinhVien { get; set; }
+        public DbSet<RegistrationModel> DanhSachDK { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Định nghĩa khóa chính cho bảng DanhSachSinhVien
+            modelBuilder.Entity<DanhSachSinhVien>()
+                .HasKey(sv => sv.MSSV); // Chỉ định MSSV là khóa chính
+        }
+    }
+}
