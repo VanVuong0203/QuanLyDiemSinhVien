475ef733b5adf8361dc3af0686c01013b90b65b3
phuchung123 <tranhung.2612va@gmail.com>
2024-12-13 20:45:56 +0700

    Hung cap nhat

--- QuanLiDiem/obj/Debug/net8.0/QuanLiDiem.AssemblyInfo.cs
+++ QuanLiDiem/obj/Debug/net8.0/QuanLiDiem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+695df8c0fa9993fa0074ed88f50d4ea623cbd92e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e465504df718d2111fd945a443f8a231e77ea61")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
