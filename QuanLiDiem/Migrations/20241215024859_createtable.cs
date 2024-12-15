bb522a1c6c9e36b8d8c4923162eab74b1d65f0e5
VanVuong0203 <huynhvanvuong0203@gmail.com>
2024-12-15 11:02:33 +0700

    them file lai

---
+++ QuanLiDiem/Migrations/20241215024859_createtable.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLiDiem.Migrations
+{
+    /// <inheritdoc />
+<<<<<<<< HEAD:QuanLiDiem/Migrations/20241208052241_creatable.cs
+    public partial class creatable : Migration
+========
+    public partial class createtable : Migration
+>>>>>>>> 71f0c2f1a1ea7a4f17f2db88f547aa0c88a7c8c8:QuanLiDiem/Migrations/20241215024859_createtable.cs
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DanhSachDK",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CCCD = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SDT = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiaChi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NganhHoc = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DTB1 = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DTB2 = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DTB3 = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    XepLoai = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DanhSachDK", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DanhSachSinhVien",
+                columns: table => new
+                {
+                    MSSV = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    GioiTinh = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CanCuocCongDan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DiaChi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MaNganh = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TenTaiKhoan = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MatKhau = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VaiTro = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DanhSachSinhVien", x => x.MSSV);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DanhSachDK");
+
+            migrationBuilder.DropTable(
+                name: "DanhSachSinhVien");
+        }
+    }
+}
--- QuanLiDiem/obj/Debug/net8.0/QuanLiDiem.AssemblyInfo.cs
+++ QuanLiDiem/obj/Debug/net8.0/QuanLiDiem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6e6afc8f7328d98899eef7f759c3ec83e501d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af5b85169960ced3aa1d3f1ba38010db67d09f04")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuanLiDiem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
